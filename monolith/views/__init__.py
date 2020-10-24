fc18e3ee0f5c7fe9981b72d1c66d6f66fdfad946
reuseman <17575915+reuseman@users.noreply.github.com>
2020-10-24 18:14:57 +0200

    Add operators in blueprints

--- monolith/views/__init__.py
+++ monolith/views/__init__.py
@@ -1,7 +1,9 @@
+import operator
 from .home import home
 from .auth import auth
 from .users import users
 from .restaurants import restaurants
+from .operators import operators
 
 
-blueprints = [home, auth, users, restaurants]
+blueprints = [home, auth, users, restaurants, operators]
