613375e1720e11432ec42fe42017172beb66cd0e
apuccia <alessandropuccia97@gmail.com>
2020-10-31 21:06:38 +0100

    fixed restaurant tests accordingly to the refactor

--- monolith/tests/test_restaurant.py
+++ monolith/tests/test_restaurant.py
@@ -34,7 +34,7 @@ def test_create_restaurant_view_is_notavailable_ha(client):
 def test_create_table_view_is_available_operator(client, db):
     helpers.create_operator(client)
     helpers.login_operator(client)
-    helpers.inser_restaurant_db(db)
+    helpers.insert_restaurant_db(db)
     q = db.session.query(Restaurant).filter_by(id=1).first()
     
     res = client.get("/operator/restaurants/" + str(q.id) + "/create_table")
@@ -43,7 +43,7 @@ def test_create_table_view_is_available_operator(client, db):
 
 def test_create_table_view_is_notavailable_anonymous(client, db):
     helpers.create_operator(client)
-    helpers.inser_restaurant_db(db)
+    helpers.insert_restaurant_db(db)
     q = db.session.query(Restaurant).filter_by(id=1).first()
     
     res = client.get("/operator/restaurants/" + str(q.id) + "/create_table")
@@ -52,7 +52,7 @@ def test_create_table_view_is_notavailable_anonymous(client, db):
 
 def test_create_table_view_is_notavailable_user(client, db):
     helpers.create_operator(client)
-    helpers.inser_restaurant_db(db)
+    helpers.insert_restaurant_db(db)
 
     helpers.create_user(client)
     helpers.login_user(client)
@@ -65,7 +65,7 @@ def test_create_table_view_is_notavailable_user(client, db):
 
 def test_create_table_view_is_notavailable_ha(client, db):
     helpers.create_operator(client)
-    helpers.inser_restaurant_db(db)
+    helpers.insert_restaurant_db(db)
     
     helpers.create_health_authority(client)
     helpers.login_authority(client)
@@ -77,6 +77,6 @@ def test_create_table_view_is_notavailable_ha(client, db):
 
 
 def test_restaurants(client, db):
-    helpers.inser_restaurant_db(db)
+    helpers.insert_restaurant_db(db)
     allrestaurants = db.session.query(Restaurant).all()
     assert len(allrestaurants) == 1
