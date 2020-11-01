ecaacf6d644b50c71ba6374c49954f6d0c8f2ff0
Tommaso Colella <tommasocolella95@gmail.com>
2020-11-01 19:45:21 +0100

    added html5 email form and date interactive form

--- monolith/services/forms.py
+++ monolith/services/forms.py
@@ -1,6 +1,7 @@
 from monolith.services.auth import authority_required
 from sys import displayhook
 from wtforms import widgets, validators
+from wtforms.fields.html5 import DateField, EmailField
 from monolith.models.precautions import Precautions
 from flask_wtf import FlaskForm
 from wtforms.ext.sqlalchemy.fields import QuerySelectMultipleField
@@ -16,11 +17,11 @@ class LoginForm(FlaskForm):
 
 
 class UserForm(FlaskForm):
-    email = f.StringField("email", validators=[DataRequired()])
+    email = EmailField("email", validators=[DataRequired(), Email()])
     firstname = f.StringField("firstname", validators=[DataRequired()])
     lastname = f.StringField("lastname", validators=[DataRequired()])
     password = f.PasswordField("password", validators=[DataRequired()])
-    dateofbirth = f.DateField("dateofbirth", format="%d/%m/%Y")
+    dateofbirth = DateField("dateofbirth", format="%d/%m/%Y")
     display = ["email", "firstname", "lastname", "password", "dateofbirth"]
 
 
@@ -50,7 +51,8 @@ class AuthorityForm(FlaskForm):
     lat = f.DecimalField("latitude", validators=[DataRequired()])
     lon = f.DecimalField("longitude", validators=[DataRequired()])
 
-    display = ["email", "name", "password", "country", "state", "city", "lat", "lon"]
+    display = ["email", "name", "password",
+               "country", "state", "city", "lat", "lon"]
 
 
 def precautions_choices():
@@ -81,7 +83,8 @@ class CreateRestaurantForm(FlaskForm):
     phone = f.IntegerField("phone", validators=[DataRequired()])
     time_of_stay = f.RadioField(
         "time_of_stay",
-        choices=[("30", "30 minutes"), ("90", "1:30 hour"), ("180", "3 hours")],
+        choices=[("30", "30 minutes"),
+                 ("90", "1:30 hour"), ("180", "3 hours")],
         validators=[DataRequired()],
     )
     prec_measures = MultiCheckboxField(
@@ -138,7 +141,8 @@ class MarkEmailForm(FlaskForm):
     )
     email = f.StringField(
         "Email",
-        validators=[DataRequired(), Email(message="Insert a valid email address.")],
+        validators=[DataRequired(), Email(
+            message="Insert a valid email address.")],
         render_kw={"placeholder": "example@mail.com"},
     )
     submit = f.SubmitField("Mark")
