9bd3c7105eb51f486ff9b32052b5e8d4422c6f18
Tommaso Colella <tommasocolella95@gmail.com>
2020-11-17 20:42:11 +0100

    fixed comment

--- monolith/views/home.py
+++ monolith/views/home.py
@@ -19,15 +19,16 @@ home = Blueprint("home", __name__)
 
 @home.route("/")
 def index():
-    restaurants = [res.__dict__
-                   for res in db.session.query(Restaurant).all()]
+    restaurants = [res.__dict__ for res in db.session.query(Restaurant).all()]
     images_path_dict = {}
     for el in restaurants:
-        print(el)
+        # print(el)
         path = "./monolith/static/uploads/" + str(el["id"])
         photos_paths = os.listdir(path)
         # gets only the first one
         if photos_paths:
             el["path"] = os.path.basename(photos_paths[0])
 
-    return render_template("index.html", restaurants=restaurants, paths=images_path_dict)
+    return render_template(
+        "index.html", restaurants=restaurants, paths=images_path_dict
+    )
