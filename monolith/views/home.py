09b9a93664e768ce5311865db2d6cb4f9cbc8f07
Tommaso Colella <tommasocolella95@gmail.com>
2020-11-17 20:06:16 +0100

    added pic loading from uploads in homepage

--- monolith/views/home.py
+++ monolith/views/home.py
@@ -2,6 +2,7 @@ from monolith.models import restaurant
 from flask import Blueprint, render_template, session, redirect, request
 from flask_login import login_required
 from flask.helpers import flash
+import os
 
 from monolith import db
 from monolith.models import Restaurant, User, Operator
@@ -18,5 +19,15 @@ home = Blueprint("home", __name__)
 
 @home.route("/")
 def index():
-    restaurants = db.session.query(Restaurant).all()
-    return render_template("index.html", restaurants=restaurants)
+    restaurants = [res.__dict__
+                   for res in db.session.query(Restaurant).all()]
+    images_path_dict = {}
+    for el in restaurants:
+        print(el)
+        path = "./monolith/static/uploads/" + str(el["id"])
+        photos_paths = os.listdir(path)
+        # gets only the first one
+        if photos_paths:
+            el["path"] = os.path.basename(photos_paths[0])
+
+    return render_template("index.html", restaurants=restaurants, paths=images_path_dict)
